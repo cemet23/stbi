5561d9a32881ac2b5d85040853fe3b18c3247142
cemet23 <43136120+cemet23@users.noreply.github.com>
2018-09-15 19:46:55 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$host='fdb21.awardspace.net';
+$user='2774446_luthfi';
+$pass='al3natore';
+$database='2774446_luthfi';
+
+$conn=mysqli_connect($host,$user,$pass);
+@mysqli_select_db($database);
+
+?>
---
+++ upload.php
@@ -0,0 +1,18 @@
+<html>
+<title>Form Upload</title>
+<body>
+	<div style="width: 100%; text-align: center;">
+<form enctype="multipart/form-data" method="POST" action="hasil_upload.php">
+File yang di upload : <input type="file" name="fupload"><br>
+Deskripsi File : <br>
+<textarea name="deskripsi" rows="8" cols="40"></textarea><br>
+<input type=submit value=Upload>
+</form>
+<br>
+<br>
+Ahmad Sobirin | 15.01.53.0003 <br>
+Muhammad Luthfi | 15.01.53.0026<br>
+Fery Andrika | 15.01.53.0027
+</div>
+</body>
+</html>
